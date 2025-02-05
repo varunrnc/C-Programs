af9d8e775f298fdc8ce12dc88820529f70decec3
varunrnc <barunpandit143@gmail.com>
2025-02-05 22:59:11 +0530

    update

--- Fibonacci Series/fibonacci_series_in_c.c
+++ Fibonacci Series/fibonacci_series_in_c.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int n1=0, n2=1, n3, i, number;
+    int n1 = 0, n2 = 1, n3, i, number;
     printf("Enter the number of elements:");
     scanf("%d", &number);
     printf("\n%d %d", n1, n2); // printing 0 and 1
-    for(i=2; i<number; ++i)
+    for (i = 2; i < number; ++i)
     {
         n3 = n1 + n2;
         printf(" %d", n3);
